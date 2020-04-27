1ae96b213a9045fa84b4a7909536737237064f43
Sonal Das <sonald@andrew.cmu.edu>
2020-04-26 23:45:24 -0400

    Added Login and signup

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
+if (process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
 const express = require('express')
 const mongoose = require('mongoose')
 const TravelPost = require('./models/post')
 const postRouter = require('./routes/posts')
 const methodOverride = require('method-override')
 const app = express()
+
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const intializePassport = require('./passport-config')
+intializePassport(
+    passport, 
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
+const users = [] //in memory storing registered user info. Use db instead
 
 mongoose.connect('mongodb://localhost/gallivanter',{
     useNewUrlParser: true, 
@@ -18,16 +34,76 @@ app.set('view engine', 'ejs') //View engine converts ejs code to html
 
 app.use(express.urlencoded({ extended: false })) //access all diff parameters from post route
 app.use(methodOverride('_method'))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.get('/', async (req, res) => {
+app.get('/', checkAuthenticated,async (req, res) => {
+
     const posts = await TravelPost.find().sort({
         createdDate: 'desc'
     })
 
     console.log("Database Entries: \n" + posts)
-    res.render('posts/index', {posts: posts})
+    res.render('posts/index', {posts: posts, name: 'Welcome ' + req.user.name})
     
 })
 
+app.get('/login', checkNotAuthenticated, (req,res) => {
+    res.render('login.ejs')
+})
+
+app.get('/register', checkNotAuthenticated, (req,res) => {
+    res.render('register.ejs')
+})
+
+app.post('/login', checkNotAuthenticated, passport.authenticate('local',{
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+
+app.post('/register', checkNotAuthenticated, async (req,res) => {
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({ //Save this info in db instead of a in-memory var
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch{
+        res.redirect('/register')
+    }
+    console.log(users)
+})
+
 app.use('/posts',postRouter) //Should come after everything else
+app.delete('/logout', (req,res) => {
+    req.logOut() //this func is passport sets up for us automatically, it will clear session and log user out.
+    res.redirect('/login')
+})
+
+function checkAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return next()
+    }
+
+    res.redirect('/login')
+}
+
+function checkNotAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+        return res.redirect('/')
+    }
+
+    next()
+}
+
 app.listen(5000)
